cb070d4561af556a657ddec20cdf0445be263b19
Qinxiang <caoqinxiang@gmail.com>
2015-02-04 16:45:15 -0500

    save to fix a bug in Rtac store.

--- mc_reify/verif_nest3.v
+++ mc_reify/verif_nest3.v
@@ -50,6 +50,31 @@ Definition multi_command_spec :=
                           ((Int.add i122 (Int.repr 4), Int.add i121 (Int.repr 3)),
                            (Int.add i112 (Int.repr 2), Int.add i111 (Int.repr 1))))) p).
 
+Definition multi_command_spec' :=
+ DECLARE _multi_command
+  WITH i111: int,
+       i112: int,
+       i121: int,
+       i122: int,
+       i211: int,
+       i212: int,
+       i221: int,
+       i222: int,
+       p: val,
+       q: val
+  PRE  []
+        PROP ()
+        LOCAL(var _p t_struct_c p)
+        SEP(`(data_at Ews t_struct_c (default_val _) q);
+            `(data_at Ews t_struct_c
+               (repinj t_struct_c (((i111, i112), (i121, i122)), ((i211, i212), (i221, i222)))) p))
+  POST [ tvoid ]
+            `(data_at Ews t_struct_c (default_val _) q) * 
+            `(data_at Ews t_struct_c
+               (repinj t_struct_c (((i111, i112), (i121, i122)),
+                          ((Int.add i122 (Int.repr 4), Int.add i121 (Int.repr 3)),
+                           (Int.add i112 (Int.repr 2), Int.add i111 (Int.repr 1))))) p).
+
 Definition multi_command_s_spec :=
  DECLARE _multi_command_s
   WITH p0: val,
@@ -62,14 +87,14 @@ Definition multi_command_s_spec :=
        p7: val
   PRE  []
         PROP ()
-        LOCAL(var _p0 t_struct_c p0;
-              var _p1 t_struct_c p1;
-              var _p2 t_struct_c p2;
-              var _p3 t_struct_c p3;
-              var _p4 t_struct_c p4;
-              var _p5 t_struct_c p5;
-              var _p6 t_struct_c p6;
-              var _p7 t_struct_c p7)
+        LOCAL(var _p0 tint p0;
+              var _p1 tint p1;
+              var _p2 tint p2;
+              var _p3 tint p3;
+              var _p4 tint p4;
+              var _p5 tint p5;
+              var _p6 tint p6;
+              var _p7 tint p7)
         SEP(`(data_at Ews tint (Vint (Int.repr 0)) p0);
             `(data_at Ews tint (Vint (Int.repr 0)) p1);
             `(data_at Ews tint (Vint (Int.repr 0)) p2);
@@ -80,14 +105,14 @@ Definition multi_command_s_spec :=
             `(data_at Ews tint (Vint (Int.repr 0)) p7))
   POST [ tvoid ]
         PROP ()
-        LOCAL(var _p0 t_struct_c p0;
-              var _p1 t_struct_c p1;
-              var _p2 t_struct_c p2;
-              var _p3 t_struct_c p3;
-              var _p4 t_struct_c p4;
-              var _p5 t_struct_c p5;
-              var _p6 t_struct_c p6;
-              var _p7 t_struct_c p7)
+        LOCAL(var _p0 tint p0;
+              var _p1 tint p1;
+              var _p2 tint p2;
+              var _p3 tint p3;
+              var _p4 tint p4;
+              var _p5 tint p5;
+              var _p6 tint p6;
+              var _p7 tint p7)
         SEP(`(data_at Ews tint (Vint (Int.repr 0)) p0);
             `(data_at Ews tint (Vint (Int.repr 0)) p1);
             `(data_at Ews tint (Vint (Int.repr 0)) p2);
@@ -97,7 +122,16 @@ Definition multi_command_s_spec :=
             `(data_at Ews tint (Vint (Int.repr 2)) p6);
             `(data_at Ews tint (Vint (Int.repr 1)) p7)).
 
-Definition Vprog : varspecs := (_p, t_struct_c)::nil.
+Definition Vprog : varspecs := (_p, t_struct_c)::
+                               (_p0, tint)::
+                               (_p1, tint)::
+                               (_p2, tint)::
+                               (_p3, tint)::
+                               (_p4, tint)::
+                               (_p5, tint)::
+                               (_p6, tint)::
+                               (_p7, tint)::
+                               nil.
 
 Definition Gprog : funspecs := 
     multi_command_s_spec::multi_command_spec::nil.
@@ -105,48 +139,10 @@ Definition Gprog : funspecs :=
 Require Import Timing.
 Clear Timing Profile.
 
-Lemma body_multi_command_s: semax_body Vprog Gprog f_multi_command_s multi_command_s_spec.
-Proof.
-  start_function.
-start_timer "Folded".
-eapply semax_seq'.
-eapply semax_seq'.
-+
-hoist_later_in_pre.
-
-eapply sc_set_load_store.semax_SC_field_load with (e1 := (Evar _p0 tint)) (efs := nil) (tts := nil)
-(gfs := nil) (gfs0 := nil) (gfs1 := nil) (e := Struct_env) (t_root := tint) (n := 0%nat) (lr := LLLL).
-reflexivity.
-reflexivity.
-reflexivity.
-Locate legal_nested_efield.
-Print compute_lr.
-reflexivity.
-
-
-
-
-
-  forward.forward. [entailer! | solve_legal_nested_field_in_entailment' |].
-  simpl upd_reptype.
-  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
-  simpl upd_reptype.
-  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
-  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
-  simpl upd_reptype.
-  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
-  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
-  simpl upd_reptype.
-  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
-  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
-  simpl upd_reptype.
-stop_timer "Folded".
-  forward.forward. (* return *)
-Qed.
 Lemma body_multi_command: semax_body Vprog Gprog f_multi_command multi_command_spec.
 Proof.
   start_function.
-start_timer "Folded".
+start_timer "08a Folded Ltac".
   forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
   simpl upd_reptype.
   forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
@@ -160,7 +156,7 @@ start_timer "Folded".
   forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
   forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
   simpl upd_reptype.
-stop_timer "Folded".
+stop_timer "08a Folded Ltac".
   forward.forward. (* return *)
 Qed.
 
@@ -174,7 +170,7 @@ Proof.
   unfold_field_at 3%nat.
   unfold_field_at 5%nat.
   unfold_field_at 7%nat.
-start_timer "Unfolded".
+start_timer "08a Unfolded Ltac".
   forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
   forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
   simpl upd_reptype.
@@ -187,7 +183,7 @@ start_timer "Unfolded".
   forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
   forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
   simpl upd_reptype.
-stop_timer "Unfolded".
+stop_timer "08a Unfolded Ltac".
   forward.forward. (* return *)
   unfold_data_at 1%nat.
   unfold_field_at 9%nat.
@@ -198,5 +194,127 @@ stop_timer "Unfolded".
   unfold_field_at 15%nat.
   entailer!.
 Qed.
+
+Lemma body_multi_command3: semax_body Vprog Gprog f_multi_command multi_command_spec'.
+Proof.
+  start_function.
+  unfold_data_at 1%nat.
+  unfold_data_at 1%nat.
+  unfold_field_at 1%nat.
+  unfold_field_at 3%nat.
+  unfold_field_at 5%nat.
+  unfold_field_at 7%nat.
+  unfold_field_at 1%nat.
+  unfold_field_at 3%nat.
+  unfold_field_at 5%nat.
+  unfold_field_at 7%nat.
+  unfold_field_at 9%nat.
+  unfold_field_at 11%nat.
+  unfold_field_at 13%nat.
+  unfold_field_at 15%nat.
+start_timer "016 Unfolded Ltac".
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  simpl upd_reptype.
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  simpl upd_reptype.
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  simpl upd_reptype.
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  simpl upd_reptype.
+stop_timer "016 Unfolded Ltac".
+  forward.forward. (* return *)
+  unfold_data_at 1%nat.
+  unfold_data_at 1%nat.
+  unfold_field_at 17%nat.
+  unfold_field_at 19%nat.
+  unfold_field_at 21%nat.
+  unfold_field_at 23%nat.
+  unfold_field_at 17%nat.
+  unfold_field_at 19%nat.
+  unfold_field_at 21%nat.
+  unfold_field_at 23%nat.
+  unfold_field_at 25%nat.
+  unfold_field_at 27%nat.
+  unfold_field_at 29%nat.
+  unfold_field_at 31%nat.
+  cancel.
+Qed.
+
+Lemma body_multi_command_s: semax_body Vprog Gprog f_multi_command_s multi_command_s_spec.
+Proof.
+  start_function.
+start_timer "08b Unfolded Ltac".
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  simpl upd_reptype.
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  simpl upd_reptype.
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  simpl upd_reptype.
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  forward.forward; [entailer! | solve_legal_nested_field_in_entailment' |].
+  simpl upd_reptype.
+stop_timer "08b Unfolded Ltac".
+  forward.forward. (* return *)
+  cancel.
+Qed.
+
+Require Export mc_reify.symexe_soundness.
+
+Require Import MirrorCore.RTac.Repeat.
+Require Import MirrorCore.RTac.Then.
+Require Import MirrorCore.RTac.Try.
+Require Import MirrorCore.RTac.First.
+Require Import MirrorCore.RTac.Fail.
+Require Import MirrorCore.RTac.Simplify.
+Require Import MirrorCore.RTac.RTac.
+Require Import mc_reify.types.
+Require Import mc_reify.funcs.
+Require Import mc_reify.func_defs.
+Require Import mc_reify.app_lemmas.
+Require Import MirrorCore.LemmaApply.
+Require Import ExtLib.Tactics.
+Require Import MirrorCore.Util.ListMapT.
+Require Import MirrorCharge.RTac.Instantiate.
+Require Import MirrorCharge.RTac.Intro.
+Require Import MirrorCharge.RTac.Apply.
+Require Import MirrorCharge.RTac.EApply.
+Require Import mc_reify.rtac_base.
+Require Import mc_reify.reified_ltac_lemmas.
+Require Import mc_reify.hoist_later_in_pre.
+Require Import mc_reify.set_load_store.
+Require Import mc_reify.symexe.
+
+Lemma body_multi_command_s2: semax_body Vprog nil f_multi_command_s multi_command_s_spec.
+Proof.
+  start_function.
+
+unfold abbreviate in Delta, (*MORE_COMMANDS,*) POSTCONDITION.
+subst Delta (*MORE_COMMANDS*) POSTCONDITION.
+ensure_normal_ret_assert.
+match goal with
+| |- semax _ _ _ (normal_ret_assert ?M) => set (POSTCONDITION := M)
+end.
+
+prepare_reify.
+unfold PTree.set, PTree.prev, tarray, tint; simpl.  repeat split.
+
+rforward.
+
+repeat eexists;
+repeat (split; repeat eexists).
++ intros; entailer!.
+  solve_legal_nested_field.
++ simpl nth_error. reflexivity.
++ entailer!.
+  solve_legal_nested_field.
+
+start_timer "08b Unfolded Rtac".
 
 Print Timing Profile.
